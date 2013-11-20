c0e8cf7bd69479216cdcd3d6b3d39b8eb1f139fe
erlaroche <elaroche@athena.att.net>
2013-11-20 16:31:34 -0600

    Add three passing

--- pizza.rb
+++ pizza.rb
@@ -1,11 +1,16 @@
 module Pizza
-  
-  class Pie
 
+
+  class Pie
+    attr_accessor :toppings
+    def initialize(toppings)
+      @toppings = toppings
+    end
   end
 
   class Topping
     attr_accessor :name, :vegetarian
+    
     # Instantiate a new topping.
     #
     # name - The String name of the topping.
